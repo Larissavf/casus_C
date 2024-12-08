c468ec429987630b0fb9204c8852edbbf8f40048
Larissavf <116642226+Larissavf@users.noreply.github.com>
2024-12-08 16:18:11 +0100

    forgotten last step

--- TGModels.py
+++ TGModels.py
@@ -4,26 +4,29 @@ class TGmodels:
     def formule(y):
         return True
 
-    def solve_rungekutta(self, y, t, steps=10):
+    def solve_rungekutta(self, y, t, lenght, steps=10):
         """Lost de differentiaalvergelijking
             dy/dt = y + 1
         met beginconditie y(0) = 0 op met Runge-Kutta's methode,
         en retourneert de waarde y(t) voor een gevraagde t.
         """
+        v = []
         dt = t / steps
-        for step in range(steps):
-            # Tijdelijke stappen:
-            dydt1 = self.formule(y)                # Differentiaalvergelijking stap 1
-            y1 = y + dydt1 * 0.5 * dt
-            dydt2 = self.formule(y1)                # Differentiaalvergelijking stap 2
-            y2 = y + dydt2 * 0.5 * dt
-            dydt3 = self.formule(y2)                # Differentiaalvergelijking stap 3
-            y3 = y + dydt3 * dt
-            dydt4 = self.formule(y3)                # Differentiaalvergelijking stap 4
-            # Definitieve stap:
-            dydt = (dydt1 + 2.0 * dydt2 + 2.0 * dydt3 + dydt4) / 6.0
-            y += dydt * dt
-        return y
+        for times in range(lenght):
+            for step in range(steps):
+                # Tijdelijke stappen:
+                dydt1 = self.formule(y)                # Differentiaalvergelijking stap 1
+                y1 = y + dydt1 * 0.5 * dt
+                dydt2 = self.formule(y1)                # Differentiaalvergelijking stap 2
+                y2 = y + dydt2 * 0.5 * dt
+                dydt3 = self.formule(y2)                # Differentiaalvergelijking stap 3
+                y3 = y + dydt3 * dt
+                dydt4 = self.formule(y3)                # Differentiaalvergelijking stap 4
+                # Definitieve stap:
+                dydt = (dydt1 + 2.0 * dydt2 + 2.0 * dydt3 + dydt4) / 6.0
+                y += dydt * dt
+            v.append(y)
+        return v
 
 
 class Lineair_growth(TGmodels):
